f8f5720dcc9d632a95f558ea4a7ab7a6808aab88
Xdaddy11 <malnikastha11@gmail.com>
2023-04-21 13:47:02 -0400

    s

---
+++ script.js
@@ -0,0 +1,20 @@
+// const fs = require("fs");
+// const http = require("http");
+// const https = require("https");
+// const bcrypt = require("bcrypt");
+
+// const HTTP_PORT = process.env.PORT || 8080;
+// const HTTPS_PORT = 4433;
+// const WEEK10ASSETS = "./week10-assets/";
+// const WEEK12ASSETS = "./week12-assets/";
+// const SSL_KEY_FILE = WEEK12ASSETS + "server.key";
+// const SSL_CRT_FILE = WEEK12ASSETS + "server.crt";
+
+// // read in the contents of the HTTPS certificate and key
+// const https_options = {
+//   key: fs.readFileSync(__dirname + "/" + SSL_KEY_FILE),
+//   cert: fs.readFileSync(__dirname + "/" + SSL_CRT_FILE),
+// };
+
+// const salt = 10;
+// const hashPw = await bcrypt.hash("paswrd", bcrypt);
